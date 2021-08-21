778930b455c12705b2404d63a6bf1c2f9eb904bd
Mathew Grabau <mgrabau@mathewgrabau.ca>
2021-08-20 22:42:31 -0500

    Implemented the event card listing and rendering the list of them.

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -8,24 +8,26 @@
 <script>
 export default {
   name: "EventCard",
-  data() {
-    return {
-      event: {
-        id: 5928101,
-        category: "animal welfare",
-        title: "Cat Adoption Day",
-        description: "Find your new feline friend at this event.",
-        location: "Meow Town",
-        date: "January 28, 2022",
-        time: "12:00",
-        petsAllowed: true,
-        organizer: "Kat Laydee",
-      },
-    };
-  },
+  props: {
+    event: Object
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid black;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
 </style>
